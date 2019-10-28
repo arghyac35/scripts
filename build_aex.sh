e1e4f20b29659f41e46cd54343e57a3c5e41b450
Arghya Chanda <arghyac35@gmail.com>
2019-10-28 10:13:53 +0530

    Change sequence of asking questions
    - also ask for aex path and update accordingly
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- build_aex.sh
+++ build_aex.sh
@@ -22,25 +22,11 @@ rer=$(tput rev)$(tput bold)$(tput setaf 1) #  bold red reversed
 txtrst=$(tput sgr0)                        #  Reset
 
 DEVICE=$1
-aex_check=$(grep -n "EXTENDED_VERSION" $SCRIPT_DIR/vendor/aosp/config/version.mk | grep -Eo '^[^:]+')
-array=( $aex_check )
-AEX_VERSION=$(sed -n ${array[0]}'p' < $SCRIPT_DIR/vendor/aosp/config/version.mk | cut -d "=" -f 2 | tr -d '[:space:]')
 
-[[ -z "${TG_BOT_API_KEY}" ]] && echo "API_KEY not defined, exiting!" && exit 1
+[[ -z "${TG_BOT_API_KEY}" ]] && echo "BOT_API_KEY not defined, exiting!" && exit 1
 function sendTG() {
 	curl -s "https://api.telegram.org/bot${TG_BOT_API_KEY}/sendmessage" --data "text=${*}&chat_id=${TG_CHAT_ID}&parse_mode=Markdown" >/dev/null
 }
-
-echo "${blu}Run repo sync?${txtrst}"
-select yn in "Yes" "No"; do
-	case $yn in
-	Yes)
-		repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-		break
-		;;
-	No) break ;;
-	esac
-done
 
 echo "${blu}Gapps or Non-gapps Build?${txtrst}"
 select yn in "gapps" "nongapps"; do
@@ -107,14 +93,25 @@ if [ "$set_upload_build" = true ]; then
 	done
 fi
 
-echo "${blu}Make clean build?${txtrst}"
-select yn in "yes" "no"; do
+echo "Please enter the path where AEX folder is located (full path ex:/home/<user>/aex)"
+read aex_path
+
+echoText "Changing dir to ${aex_path}"
+cd $aex_path
+
+# Check aex version
+aex_check=$(grep -n "EXTENDED_VERSION" $SCRIPT_DIR/vendor/aosp/config/version.mk | grep -Eo '^[^:]+')
+array=($aex_check)
+AEX_VERSION=$(sed -n ${array[0]}'p' <$SCRIPT_DIR/vendor/aosp/config/version.mk | cut -d "=" -f 2 | tr -d '[:space:]')
+
+echo "${blu}Run repo sync?${txtrst}"
+select yn in "Yes" "No"; do
 	case $yn in
-	yes)
-		. build/envsetup.sh && make clean
+	Yes)
+		repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 		break
 		;;
-	no) break ;;
+	No) break ;;
 	esac
 done
 
@@ -131,10 +128,24 @@ select yn in "eng" "userdebug"; do
 		;;
 	esac
 done
+
+echo "${blu}Make clean build?${txtrst}"
+select yn in "yes" "no"; do
+	case $yn in
+	yes)
+		cd $aex_path
+		. build/envsetup.sh && make clean
+		break
+		;;
+	no) break ;;
+	esac
+done
+
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export CCACHE_COMPRESS=1
 export CCACHE_MAXSIZE=50G # 50 GB
+cd $aex_path
 if ! lunch "${device_build_type:?}"; then
 	echo "Lunching $DEVICE failed"
 	exit 1
@@ -144,28 +155,30 @@ echo "${blu}Please confirm do you want to start build?${txtrst}"
 select yn in "yes" "no"; do
 	case $yn in
 	yes)
-		echo "${ylw}Initiate build for ${DEVICE}...${txtrst}"
+		cd $aex_path
+		echo "${ylw}Initiating AEX ${AEX_VERSION} build for ${DEVICE}...${txtrst}"
 		if ! mka aex; then
 			echo "$DEVICE Build failed"
 			exit 1
-		fi
-		cout
-		ZIP=$(ls AospExtended-${AEX_VERSION}-$DEVICE-*.zip)
-		ZIP_SIZE="$(du -h "${ZIP}" | awk '{print $1}')"
-		MD5="$(md5sum "${FILE}" | awk '{print $1}')"
-		if [ "$set_upload_build" = true -a "$set_upload_build_server" == "gdrive" ]; then
-			# Upload file
-			GDRIVE_UPLOAD_URL=$(gdrive upload --share $ZIP | awk '/https/ {print $7}')
-			GDRIVE_UPLOAD_ID="$(echo "${GDRIVE_UPLOAD_URL}" | sed -r -e 's/(.*)&export.*/\1/' -e 's/https.*id=(.*)/\1/' -e 's/https.*\/d\/(.*)\/view/\1/')"
-			UPLOAD_INFO="
+		else
+			cd $aex_path/out/target/product/$DEVICE
+			ZIP=$(ls AospExtended-${AEX_VERSION}-$DEVICE-*.zip)
+			ZIP_SIZE="$(du -h "${ZIP}" | awk '{print $1}')"
+			MD5="$(md5sum "${FILE}" | awk '{print $1}')"
+			if [ "$set_upload_build" = true -a "$set_upload_build_server" == "gdrive" ]; then
+				# Upload file
+				GDRIVE_UPLOAD_URL=$(gdrive upload --share $ZIP | awk '/https/ {print $7}')
+				GDRIVE_UPLOAD_ID="$(echo "${GDRIVE_UPLOAD_URL}" | sed -r -e 's/(.*)&export.*/\1/' -e 's/https.*id=(.*)/\1/' -e 's/https.*\/d\/(.*)\/view/\1/')"
+				UPLOAD_INFO="
 File: [$(basename "${ZIP}")](${GDRIVE_UPLOAD_URL})
 Size: ${ZIP_SIZE}
 MD5: \`${MD5}\`
 GDrive ID: \`${GDRIVE_UPLOAD_ID}\`
 "
-			sendTG "${UPLOAD_INFO}"
-		else
-			sendTG "$DEVICE build is done."
+				sendTG "${UPLOAD_INFO}"
+			else
+				sendTG "$DEVICE build is done."
+			fi
 		fi
 		break
 		;;
